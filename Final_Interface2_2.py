5b4b773ef8618c48fc608bfb2d4e6c37935542c6
Daniel Cala <daniel.cala117@gmail.com>
2024-08-06 11:56:22 -0500

    Cambiando forma de mostrar las predicciones, de paralela a sobrepuesta

--- Final_Interface2_2.py
+++ Final_Interface2_2.py
@@ -1,83 +1,36 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QFileDialog
-from PyQt5.QtGui import QPixmap
-import cv2
 import numpy as np
+from PIL import Image, ImageDraw, ImageEnhance, ImageQt
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QFileDialog, QSlider
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import Qt
 import torch
 import torchvision.transforms as transforms
 import segmentation_models_pytorch as smp
-import matplotlib.pyplot as plt
-from PyQt5 import QtCore, QtGui, QtWidgets
-
-
-class Ui_btn_load_image_Interface(object):
-    def setupUi(self, btn_load_image):
-        btn_load_image.setObjectName("btn_load_image")
-        btn_load_image.resize(1077, 836)
-        self.InfoLabel = QtWidgets.QLabel(btn_load_image)
-        self.InfoLabel.setGeometry(QtCore.QRect(60, 500, 251, 291))
-        self.InfoLabel.setText("")
-        self.InfoLabel.setObjectName("InfoLabel")
-        self.label = QtWidgets.QLabel(btn_load_image)
-        self.label.setGeometry(QtCore.QRect(290, 40, 491, 101))
-        font = QtGui.QFont()
-        font.setFamily("MS Gothic")
-        font.setPointSize(48)
-        self.label.setFont(font)
-        self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(btn_load_image)
-        self.label_2.setGeometry(QtCore.QRect(20, 120, 1041, 71))
-        font = QtGui.QFont()
-        font.setPointSize(14)
-        self.label_2.setFont(font)
-        self.label_2.setObjectName("label_2")
-        self.label_3 = QtWidgets.QLabel(btn_load_image)
-        self.label_3.setGeometry(QtCore.QRect(400, 180, 271, 31))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        self.label_3.setFont(font)
-        self.label_3.setObjectName("label_3")
-        self.label_4 = QtWidgets.QLabel(btn_load_image)
-        self.label_4.setGeometry(QtCore.QRect(410, 220, 241, 16))
-        font = QtGui.QFont()
-        font.setPointSize(14)
-        self.label_4.setFont(font)
-        self.label_4.setObjectName("label_4")
-
-        self.retranslateUi(btn_load_image)
-        QtCore.QMetaObject.connectSlotsByName(btn_load_image)
-
-    def retranslateUi(self, btn_load_image):
-        _translate = QtCore.QCoreApplication.translate
-        btn_load_image.setWindowTitle(_translate("btn_load_image", "Dialog"))
-        self.label.setText(_translate("btn_load_image", "CANNAVANA S.A.S"))
-        self.label_2.setText(_translate("btn_load_image", "SISTEMA DE SEGUIMIENTO DE CRECIMIENTO DE CULTIVO DE CANNABIS UTILIZANDO TECNICAS DE VISION ARTIFICIAL"))
-        self.label_3.setText(_translate("btn_load_image", "JOSÉ DANIEL CALA SUÁREZ"))
-        self.label_4.setText(_translate("btn_load_image", "INGENIERÍA MECATRÓNICA"))
-
 
 class GuideUserInterface(QWidget):
-    def __init__(self):
+    def __init__(self, model_path):
         super().__init__()
         self.title = 'Inferencia con U-Net'
         self.left = 0
         self.top = 0
         self.width = 1280
-        self.height = 720  
+        self.height = 720
+        self.model_path = model_path
         self.initUI()
         self.loadModel()
+        self.mask_alpha = 0.3
+        self.mask_visible = True
+        self.predicted_mask = None
 
     def calculate_class_pixels(self, predicted_masks_np):
-        class_pixels = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}  # 
-
+        class_pixels = {i: 0 for i in range(6)}
         for pixel_value in np.unique(predicted_masks_np):
             if pixel_value in class_pixels:
                 class_pixels[pixel_value] = np.sum(predicted_masks_np == pixel_value)
-
         total_pixels = np.sum(list(class_pixels.values()))
-
         return class_pixels, total_pixels
-    
+
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
@@ -86,16 +39,27 @@ class GuideUserInterface(QWidget):
         self.label.move(20, 20)
         self.label.resize(640, 480)
 
-        self.label_info = QLabel(self)  
+        self.label_info = QLabel(self)
         self.label_info.move(50, 350)
         self.label_info.resize(640, 480)
 
         self.btn = QPushButton('Cargar imagen', self)
         self.btn.move(600, 300)
+        self.btn.clicked.connect(self.loadImage)
+
+        self.toggle_button = QPushButton('Toggle Mask', self)
+        self.toggle_button.move(800, 300)
+        self.toggle_button.clicked.connect(self.toggle_mask)
+
+        self.slider = QSlider(Qt.Orientation.Horizontal, self)
+        self.slider.setGeometry(800, 350, 160, 30)
+        self.slider.setMinimum(0)
+        self.slider.setMaximum(100)
+        self.slider.setValue(30)
+        self.slider.valueChanged.connect(self.update_transparency)
 
         self.show()
 
-    
     def loadModel(self):
         num_classes = 6
         self.model_inference = smp.Unet(
@@ -104,108 +68,123 @@ class GuideUserInterface(QWidget):
             in_channels=3,
             classes=num_classes
         )
-        state_dict = torch.load("model_Unet__41_checkpoint_epoch_40.pt", map_location=torch.device('cpu'))
-        self.model_inference.load_state_dict(state_dict)
-        self.model_inference.eval()
+        try:
+            print("Loading model...")
+            state_dict = torch.load(self.model_path, map_location=torch.device('cpu'))
+            self.model_inference.load_state_dict(state_dict)
+            self.model_inference.eval()
+            print("Model loaded successfully.")
+        except Exception as e:
+            print(f"Error loading model: {e}")
 
     def updateInfoLabel(self, class_pixels, class_percentages):
         info_text = "Píxeles de Clase:\n"
         for class_name, pixels in class_pixels.items():
             info_text += f"{class_name}: {pixels} pixels\n"
-
         info_text += "\nPorcentajes de clase:\n"
         for class_name, percentage in class_percentages.items():
-            info_text += f"{class_name}: {percentage}%\n"
-
+            info_text += f"{class_name}: {percentage:.2f}%\n"
         self.label_info.setText(info_text)
 
     def loadImage(self):
         fileName, _ = QFileDialog.getOpenFileName(self, "Abrir Imagen", "", "Image Files (*.png *.jpg *.jpeg *.bmp)")
         if fileName:
-            image = cv2.imread(fileName)
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            image_for_inference = transforms.ToPILImage()(image)
+            try:
+                print(f"Loading image: {fileName}")
+                image = Image.open(fileName).convert('RGB')
+                self.image = image
 
-            preprocess = transforms.Compose([
-            transforms.Resize((1024, 1024)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-            ])
-            preprocess2 = transforms.Compose([
-            transforms.Resize((1024, 1024)),
-            transforms.ToTensor()
-            ])
+                preprocess = transforms.Compose([
+                    transforms.Resize((1024, 1024)),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                ])
 
-            input_image = preprocess(image_for_inference).unsqueeze(0)
-            imagen_or = preprocess2(image_for_inference).unsqueeze(0)
+                input_image = preprocess(image).unsqueeze(0)
+                print("Performing inference...")
+                with torch.no_grad():
+                    output = self.model_inference(input_image)
+                predicted_masks = torch.argmax(output, dim=1)
+                predicted_masks_np = predicted_masks.squeeze(0).numpy().astype(np.uint8)
+                self.predicted_mask = predicted_masks_np
 
-            with torch.no_grad():
-                output = self.model_inference(input_image)
+                class_pixels, total_pixels = self.calculate_class_pixels(predicted_masks_np)
+                class_names = ['Fondo', 'Plantas Sanas', 'Botritis Etapa 1', 'Botritis Etapa 2', 'Botritis Etapa 3', 'Deficiencias Nutricionales']
+                class_percentages = {class_names[key]: value / total_pixels * 100 for key, value in class_pixels.items()}
+                self.updateInfoLabel(class_pixels, class_percentages)
 
-            predicted_masks = torch.argmax(output, dim=1)
+                self.display_image_with_mask(self.image, self.predicted_mask)
+                print("Image loaded and processed successfully.")
+            except Exception as e:
+                print(f"Error loading image: {e}")
 
-            input_image_np = input_image.squeeze(0).permute(1, 2, 0).numpy()
-            imagen_or = imagen_or.squeeze(0).permute(1, 2, 0).numpy()
-            predicted_masks_np = predicted_masks.squeeze(0).numpy()
+    def display_image_with_mask(self, image, mask):
+        try:
+            print("Displaying image with mask...")
+            overlay_img = self.overlay_mask_on_image(image, mask)
+            overlay_img = overlay_img.convert("RGB")
+            overlay_img = overlay_img.resize((640, 480), Image.Resampling.LANCZOS)
+            overlay_img_qt = ImageQt.ImageQt(overlay_img)
+            pixmap = QPixmap.fromImage(overlay_img_qt)
+            self.label.setPixmap(pixmap)
+        except Exception as e:
+            print(f"Error displaying image with mask: {e}")
 
-            class_pixels, total_pixels = self.calculate_class_pixels(predicted_masks_np)
+    def overlay_mask_on_image(self, image, mask):
+        try:
+            print("Overlaying mask on image...")
+            image_resized = image.resize((1024, 1024))
+            class_colors = {
+                0: (0, 0, 0, 0),  # Fondo
+                1: (0, 255, 0, int(self.mask_alpha * 255)),  # Plantas Sanas
+                2: (165, 42, 42, int(self.mask_alpha * 255)),  # Botritis Etapa 1
+                3: (128, 0, 128, int(self.mask_alpha * 255)),  # Botritis Etapa 2
+                4: (255, 165, 0, int(self.mask_alpha * 255)),  # Botritis Etapa 3
+                5: (255, 255, 0, int(self.mask_alpha * 255)),  # Deficiencias Nutricionales
+            }
+            mask_img = Image.new("RGBA", (1024, 1024))
+            mask_pixels = mask_img.load()
+            for y in range(1024):
+                for x in range(1024):
+                    mask_pixels[x, y] = class_colors[mask[y, x]]
+            overlay_img = Image.alpha_composite(image_resized.convert("RGBA"), mask_img)
+            return overlay_img
+        except Exception as e:
+            print(f"Error overlaying mask on image: {e}")
+            return image
 
-            class_names = ['Fondo', 'Plantas Sanas', 'Botritis Etapa 1', 'Botritis Etapa 2', 'Botritis Etapa 3', 'Deficiencias Nutricionales']
-            class_colors = ['black', 'lime', 'brown', 'purple', 'orange', 'yellow']
+    def toggle_mask(self):
+        try:
+            print("Toggling mask visibility...")
+            self.mask_visible = not self.mask_visible
+            if self.mask_visible:
+                self.display_image_with_mask(self.image, self.predicted_mask)
+            else:
+                self.display_image(self.image)
+        except Exception as e:
+            print(f"Error toggling mask: {e}")
 
-            cmap = plt.cm.colors.ListedColormap(class_colors) 
+    def update_transparency(self, value):
+        try:
+            print(f"Updating transparency: {value}")
+            self.mask_alpha = value / 100
+            if self.mask_visible:
+                self.display_image_with_mask(self.image, self.predicted_mask)
+        except Exception as e:
+            print(f"Error updating transparency: {e}")
 
-            fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 5))
-            #fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-            ax1.imshow(imagen_or)
-            ax1.axis('off')
-            ax1.set_title('Imagen Original')
-
-            im = ax2.imshow(predicted_masks_np, cmap=cmap, vmin=0, vmax=len(class_names)-1)
-            ax2.axis('off')
-            ax2.set_title('Máscara Predicha')
-
-            cbar = plt.colorbar(im, ax=[ax2, ax3], ticks=np.arange(len(class_names)), boundaries=np.arange(len(class_names)+1) -0.5)
-            cbar.set_ticklabels(class_names)
-
-            #cbar = plt.colorbar(im, ax=ax2, ticks=np.arange(len(class_names)-1)+1, boundaries=np.arange(len(class_names)-1) + 1)
-            #cbar.set_ticklabels(class_names[1:])
-
-            plt.tight_layout()
-
-            class_percentages = {class_names[key]: value / total_pixels * 100 for key, value in class_pixels.items()}
-            self.updateInfoLabel(class_pixels, class_percentages)
-
-            plt.show()
-
-
-
-class Ui_btn_load_image(Ui_btn_load_image_Interface, GuideUserInterface):
-    def setupUi(self, btn_load_image):
-        super().setupUi(btn_load_image)
-        self.loadModel()
-        self.btn.clicked.connect(self.loadImage)
-
-        self.InfoLabel = self.label_info
-        self.label = self.label_2
-
-        self.retranslateUi(btn_load_image)
-        QtCore.QMetaObject.connectSlotsByName(btn_load_image)
-
-    def retranslateUi(self, btn_load_image):
-        _translate = QtCore.QCoreApplication.translate
-        btn_load_image.setWindowTitle(_translate("btn_load_image", "Dialog"))
-        self.label.setText(_translate("btn_load_image", "CANNAVANA S.A.S"))
-        self.label_2.setText(_translate("btn_load_image", "SISTEMA DE SEGUIMIENTO DE CRECIMIENTO DE CULTIVO DE CANNABIS UTILIZANDO TECNICAS DE VISION ARTIFICIAL"))
-        self.label_3.setText(_translate("btn_load_image", "JOSÉ DANIEL CALA SUÁREZ"))
-        self.label_4.setText(_translate("btn_load_image", "INGENIERÍA MECATRÓNICA"))
-
+    def display_image(self, image):
+        try:
+            print("Displaying image without mask...")
+            image = image.convert("RGB")
+            image = image.resize((640, 480), Image.Resampling.LANCZOS)
+            image_qt = ImageQt.ImageQt(image)
+            pixmap = QPixmap.fromImage(image_qt)
+            self.label.setPixmap(pixmap)
+        except Exception as e:
+            print(f"Error displaying image: {e}")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    btn_load_image = QWidget()
-    ui = Ui_btn_load_image()
-    ui.setupUi(btn_load_image)
-    btn_load_image.show()
-    sys.exit(app.exec_())
-
+    ex = GuideUserInterface("model_Unet__41_checkpoint_epoch_40.pt")  # Update the path to your model
+    sys.exit(app.exec())
